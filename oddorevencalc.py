004b9489e3e984f8058b1e1d905dd90d8afba41f
genewaderivas <genewade.rivas@smc.pshs.edu.ph>
2026-09-08 15:35:06 +0800

    Create oddorevencalc.py

---
+++ oddorevencalc.py
@@ -0,0 +1,9 @@
+#This ask the user the number
+number = int(input("Enter a whole nuumber: "))
+
+
+if  number % 2 == 0:
+  print("Number is even") #output is even
+else:
+  print("Number is odd")  #output is odd
+  
